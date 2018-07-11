6edf5b1752655d7a893a8bae4b68d8a82e63b653
Elijah Kotyluk <elijah@elijahkotyluk.com>
2018-07-10 18:49:18 -0700

    Add API requests to main.js file.

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,3 +1,38 @@
+// Makes a 'GET' request to the API and returns data through an XMLHttpRequest
+function searchRecipe(){
+
+    // Request object.
+    var xhr = new XMLHttpRequest();
+    // Initialize/reinitialize existing 'GET' request to the API.
+    xhr.open('GET', "https://api.edamam.com/search?q=chicken&app_id=7919900d&app_key=47fe0acea0eb3f17386dadb3b722e233", true);
+    // .withCredentials takes a boolean. Indicates that 'cross-site' requests should be made with credentials/cookies.
+    xhr.withCredentials = true;
+    // .onload triggers when request is complete.
+    xhr.onload = function () {
+        // Logs response to console in json format.
+        console.log(xhr.responseText);
+    };
+    // Sends the request.
+    xhr.send();
+}
+
+// Makes a 'GET' request to the API and returns data through an XMLHttpRequest.
+function getNutrition() {
+
+    // Request object.
+    var xhr = new XMLHttpRequest();
+    // Initialize/Reinitialize existing 'GET' request to the API.
+    xhr.open('GET', "", true);
+    // Make requests with cookies/credentials
+    xhr.withCredentials = true;
+    // When request is complete logs the response.
+    xhr.onload = function () {
+        console.log(xhr.responseText);
+    };
+    // Send request.
+    xhr.send();
+}
+
 $(document).ready(function() {
 
     function alertinput () {

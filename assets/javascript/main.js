03fb08c36ad8717ea33c32304efd6ce0d940f4bd
Elijah Kotyluk <elijah@elijahkotyluk.com>
2018-07-12 19:35:24 -0700

    Add nutrition search function for nutrition-search button.

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,69 +1,60 @@
-$(document).ready(function() {
+// Click event handler for recipe-button.
+$('#recipe-button').on('click', function (event) {
+    // Prevents default event action.
+    event.preventDefault();
+    // Saves the user's in the ingrInput variable.
+    const ingrInput = $('#ingredients-input').val().trim();
 
-// Makes a 'GET' request to the API and returns data through an XMLHttpRequest
-function searchRecipe(){
+    // API url for making 'GET' request to find recipes.
+    const recipeUrl = "https://api.edamam.com/search?q=" + ingrInput + "&app_id=7919900d&app_key=47fe0acea0eb3f17386dadb3b722e233";
 
-    // Request object.
-    var xhr = new XMLHttpRequest();
-    // Initialize/reinitialize existing 'GET' request to the API.
-    xhr.open('GET', "https://api.edamam.com/search?q=chicken&app_id=7919900d&app_key=47fe0acea0eb3f17386dadb3b722e233", true);
-    // .withCredentials takes a boolean. Indicates that 'cross-site' requests should be made with credentials/cookies.
-    xhr.withCredentials = true;
-    // .onload triggers when request is complete.
-    xhr.onload = function () {
-        // Logs response to console in json format.
-        console.log(xhr.responseText);
-    };
-    // Sends the request.
-    xhr.send();
-};
+    // Makes a 'GET' request to the API and returns data through an XMLHttpRequest
+    function searchRecipe(){
 
-// Makes a 'GET' request to the API and returns data through an XMLHttpRequest.
-function getNutrition() {
-
-    // Request object.
-    var xhr = new XMLHttpRequest();
-    // Initialize/Reinitialize existing 'GET' request to the API.
-    xhr.open('GET', 'https://api.edamam.com/api/food-database/parser?ingr=white%20corn&app_id=9e4c6c3d&app_key=8954976b9cc76c3cf1396f60f23e75ab', true);
-    // Make requests with cookies/credentials
-    xhr.withCredentials = true;
-    // When request is complete logs the response.
-    xhr.onload = function () {
-        console.log(xhr.responseText);
-    };
-    // Send request.
-    xhr.send();
-};
-
-
-    var ingredients = [];
-
-    function alertinput () {
-        var groceries = $(this).attr('ingred-list');
-            console.log(groceries);
-    };
-    
-
-    function renderButtons() {
-        $('#ingredients-btn').empty();
-
-        for (var i = 0; i < ingredients.length; i++) {
-            var newBtn = $('<button>');
-            newBtn.addClass('ingredButtons');
-            newBtn.attr('ingred-list', ingredients[i]);
-            newBtn.text(ingredients[i]);
-            $('#ingredients-btn').append(newBtn);
+        // Request object.
+        const xhr = new XMLHttpRequest();
+        // Initialize/reinitialize existing 'GET' request to the API.
+        xhr.open('GET', recipeUrl, true);
+        // .withCredentials takes a boolean. Indicates that 'cross-site' requests should be made with credentials/cookies.
+        xhr.withCredentials = true;
+        // .onload triggers when request is complete.
+        xhr.onload = function () {
+            // Logs response to console in json format.
+            console.log(xhr.responseText);
         };
-        $('#ingredients-input').val('');
+        // Sends the request.
+        xhr.send();
     };
+    // Calls the searchRecipe() function.
+    searchRecipe();
+});
+
+ 
+// Click event handler for nutrition-button.
+$('#nutrition-button').on('click', function (event) {
+    event.preventDefault();
+
+    // Saves user input in the nutrInput variable.
+    const nutrInput = $('#nutrition-input').val().trim();
     
-    $('#add-ingredients-btn').on('click', function(event){
-        event.preventDefault();
-        var ingredient = $('#ingredients-input').val().trim();
-        ingredients.push(ingredient);
-        renderButtons();
-    });
-    
-    $(document).on('click', '.ingredButtons', alertinput);
-        renderButtons(); 
-    });
+    // API url for making 'GET' request for nutrition values.
+    const nutrUrl = "https://api.edamam.com/api/food-database/parser?ingr=" + nutrInput + "&app_id=9e4c6c3d&app_key=8954976b9cc76c3cf1396f60f23e75ab";
+
+    // Makes a 'GET' request to the API and returns data through an XMLHttpRequest.
+    function getNutrition() {
+
+        // Request object.
+        const xhr = new XMLHttpRequest();
+        // Initialize/Reinitialize existing 'GET' request to the API.
+        xhr.open('GET', nutrUrl, true);
+        // Make requests with cookies/credentials
+        xhr.withCredentials = true;
+        // When request is complete logs the response.
+        xhr.onload = function () {
+            console.log(xhr.responseText);
+        };
+        // Send request.
+        xhr.send();
+    };
+    getNutrition();
+});

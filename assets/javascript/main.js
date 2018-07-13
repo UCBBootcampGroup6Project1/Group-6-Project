a4e020aaa6b9ff81e9eb146f8019c745b07f80e8
Elijah Kotyluk <elijah@elijahkotyluk.com>
2018-07-13 08:55:51 -0700

    Add createImg function, Add createLabel function. Fix id and class names in index.html. Fix id and class names in style.css.

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,3 +1,25 @@
+// Create <img> element inside .recipe-container.
+function createImg(src) {
+    // Create <img> element.
+    var element = document.createElement('img');
+        // Set <img> src to the image link in the API response.
+        element.src = src;
+
+        return element;
+};
+
+// Create <a> element inside .recipe-container with link to the recipe.
+function createLabel(label, link) {
+    // Create <a> element.
+    var element = document.createElement('a');
+        // Set the html of the element to the label in the API response.
+        element.innerHTML = label;
+        // Set the href attribute to the link in the API response.
+        element.setAttribute('href', link);
+
+        return element;
+};
+
 // Click event handler for recipe-button.
 $('#recipe-button').on('click', function (event) {
     // Prevents default event action.
@@ -22,11 +44,26 @@ $('#recipe-button').on('click', function (event) {
             var response = JSON.parse(xhr.response);
             // Logs response to console in json format.
             console.log(response);
-            response.hits.forEach(function(hit){
-                var newDiv = document.createElement("div");
-                newDiv.innerHTML = hit.recipe.image;
-                document.body.appendChild(newDiv);
+            // Loop through the response hits(like-array).
+            response.hits.forEach(function(hit) {
+                // Create div inside itemBox variable.
+                var itemBox = document.createElement('div');
+                // Set itemBox class to '.recipe-container'.
+                itemBox.setAttribute('class', 'recipe-container');
+                // Append a label and link to the '.recipe-container'.
+                itemBox.appendChild(createLabel(hit.recipe.label, hit.recipe.url));
+                // Append an image to the '.recipe-container'.
+                itemBox.appendChild(createImg(hit.recipe.image));
+                
+                // Append '.recipe-container' to '.display-recipe' div.
+                document.getElementById('display-recipe').appendChild(itemBox);
             });
+           /* response.hits.forEach(function(hit) {
+                var recipeImg = document.createElement('img');
+                recipeImg.setAttribute('class', 'recipe-item');
+                recipeImg.src = hit.recipe.img;
+                document.getElementsByClassname('recipe-container').appendChild(recipeImg);
+            })*/
         };
         // Sends the request.
         xhr.send();
@@ -34,7 +71,6 @@ $('#recipe-button').on('click', function (event) {
     // Calls the searchRecipe() function.
     searchRecipe();
 });
-
  
 // Click event handler for nutrition-button.
 $('#nutrition-button').on('click', function (event) {
@@ -56,13 +92,24 @@ $('#nutrition-button').on('click', function (event) {
         // Make requests with cookies/credentials
         xhr.withCredentials = true;
         // When request is complete logs the response.
+        
         xhr.onload = function () {
+            if(xhr.status === 400 || xhr.status === 404) {
+                alert('Choose a food item.');
+            }
             var response = JSON.parse(xhr.response);
-            console.log(JSON.parse(xhr.response));
-            console.log('food uri', response.hints[0].food.uri);
+            console.log(response.hints[0].food.nutrients);
+            //var newDiv = document.createElement("div");
+            //newDiv.innerHTML = response.hints[0].food.nutrients;
+            document.getElementById('display-nutrition').innerHTML = 'Calories: ' + response.hints[0].food.nutrients.ENERC_KCAL + '<br>' +
+                'Protein: ' + response.hints[0].food.nutrients.PROCNT + 'g' + '<br>' +
+                'Fat: ' + response.hints[0].food.nutrients.FAT + 'g' + '<br>' +
+                'Carbs: ' + response.hints[0].food.nutrients.CHOCDF + '%';
+
         };
         // Send request.
         xhr.send();
     };
     getNutrition();
 });
+
